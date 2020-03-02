99d8308786f08cd8e4329a95ce31b2de05ce84a8
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-03-02 12:44:51 +1100

    Initial attempt at interactive branch switcher
    Based largely off of https://gist.github.com/brycehanscomb/2717d21b25a491c9f4c0bd8be31d13d3
    this implements an interactive branch switcher. There is a lot of
    cleanup and features to be implemented before it's at parity with the
    gist, but this initial attempt is functional.

--- main.go
+++ main.go
@@ -1,17 +1,76 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	cmd := exec.Command("git", "branch")
-	stdoutStderr, err := cmd.CombinedOutput()
+	gitBranchesCmd := exec.Command("git", "branch", "--list")
+	gitBranchesStdoutStderr, gitBranchesErr := gitBranchesCmd.CombinedOutput()
+
+	if gitBranchesErr != nil {
+		log.Fatal(gitBranchesErr)
+	}
+
+	gitCurrentBranchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	gitCurrentBranchStdoutStderr, gitCurrentBranchErr := gitCurrentBranchCmd.CombinedOutput()
+
+	if gitCurrentBranchErr != nil {
+		log.Fatal(gitCurrentBranchErr)
+	}
+
+	fmt.Printf("all branches: %q\n", gitBranchesStdoutStderr)
+	fmt.Printf("current branch: %q\n", gitCurrentBranchStdoutStderr)
+
+	// split all branches on \n
+	branches := strings.Split(bytesToString(gitBranchesStdoutStderr), "\n")
+	// remove the * from the current branch
+
+	fmt.Printf("total amount of branches is: %d\n", len(branches))
+	// map each branch with it's index
+	for index, element := range branches {
+		fmt.Println("mapping the branches:")
+		if len(element) > 0 {
+			fmt.Printf("%d) %v\n", index, element)
+		}
+	}
+
+	// listen for input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("input: %v", text)
+	// if input matches a branch number:
+	inputInt, err := strconv.Atoi(strings.TrimSuffix(text, "\n"))
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%q\n", stdoutStderr)
+
+	branchToCheckout := branches[inputInt]
+
+	fmt.Println("branch to checkout:")
+	fmt.Println(branchToCheckout)
+	fmt.Println(len(branchToCheckout))
+	fmt.Println(len(strings.TrimSpace(branchToCheckout)))
+	// checkout that branch
+	gitCheckoutCmd := exec.Command("git", "checkout", strings.TrimSpace(branchToCheckout))
+	gitCheckoutStdStderr, gitCheckoutErr := gitCheckoutCmd.CombinedOutput()
+
+	if gitCheckoutErr != nil {
+		log.Fatal(gitCheckoutErr)
+	}
+
+	fmt.Printf("blah: %q", gitCheckoutStdStderr)
+}
+
+// https://gist.github.com/is73/de4f38e1d8da157fe33e
+func bytesToString(data []byte) string {
+	return string(data[:])
 }
