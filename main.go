d40cc4dfba4fc529ef6553c91af31c8138a3a5ab
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-03-02 14:11:26 +1100

    Add skeleton for the show command

--- main.go
+++ main.go
@@ -8,9 +8,19 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("git", "branch")
-	stdoutStderr, err := cmd.CombinedOutput()
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "show",
+				Usage: "[INCOMPLETE] shows the local and remote branches. Can switch to the branch by entering the number",
+				Action: func(c *cli.Context) error {
+					return cli.Exit("Still being built!", 1)
+				},
+			},
+		},
+	}
 
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
