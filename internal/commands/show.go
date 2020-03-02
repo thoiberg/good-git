2c03829e5dfbdeae8f6a2a2b2b90226fd38d3c7b
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-03-03 00:46:23 +1100

    Update error handling to print out stderr
    Git's error messages are the best feedback we have for now, so print
    that to screen if a problem occurs.

--- internal/commands/show.go
+++ internal/commands/show.go
@@ -15,7 +15,7 @@ func Show() {
 	gitBranchesStdoutStderr, gitBranchesErr := gitBranchesCmd.CombinedOutput()
 
 	if gitBranchesErr != nil {
-		log.Fatal(gitBranchesErr)
+		log.Fatal(bytesToString(gitBranchesStdoutStderr))
 	}
 
 	gitCurrentBranchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
@@ -25,17 +25,10 @@ func Show() {
 		log.Fatal(gitCurrentBranchErr)
 	}
 
-	fmt.Printf("all branches: %q\n", gitBranchesStdoutStderr)
-	fmt.Printf("current branch: %q\n", gitCurrentBranchStdoutStderr)
-
-	// split all branches on \n
 	branches := strings.Split(bytesToString(gitBranchesStdoutStderr), "\n")
-	// remove the * from the current branch
+	// TODO: remove the * from the current branch
 
-	fmt.Printf("total amount of branches is: %d\n", len(branches))
-	// map each branch with it's index
 	for index, element := range branches {
-		fmt.Println("mapping the branches:")
 		if len(element) > 0 {
 			fmt.Printf("%d) %v\n", index, element)
 		}
@@ -45,29 +38,23 @@ func Show() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter text: ")
 	text, _ := reader.ReadString('\n')
-	fmt.Printf("input: %v", text)
-	// if input matches a branch number:
+
 	inputInt, err := strconv.Atoi(strings.TrimSuffix(text, "\n"))
+	// TODO: Need to handle when the input is not within the valid range
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	branchToCheckout := branches[inputInt]
-
-	fmt.Println("branch to checkout:")
-	fmt.Println(branchToCheckout)
-	fmt.Println(len(branchToCheckout))
-	fmt.Println(len(strings.TrimSpace(branchToCheckout)))
-	// checkout that branch
 	gitCheckoutCmd := exec.Command("git", "checkout", strings.TrimSpace(branchToCheckout))
 	gitCheckoutStdStderr, gitCheckoutErr := gitCheckoutCmd.CombinedOutput()
 
 	if gitCheckoutErr != nil {
-		log.Fatal(gitCheckoutErr)
+		log.Fatal(bytesToString(gitCheckoutStdStderr))
 	}
 
-	fmt.Printf("blah: %q", gitCheckoutStdStderr)
+	fmt.Print(bytesToString(gitCheckoutStdStderr))
 }
 
 // https://gist.github.com/is73/de4f38e1d8da157fe33e
