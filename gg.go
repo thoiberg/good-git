073f1ea3fa767290f322cd8abd3d5da732e3ec7d
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-03-05 13:20:50 +1100

    Remove incomplete status from complete commands

--- gg.go
+++ gg.go
@@ -18,7 +18,7 @@ func main() {
 		Commands: []*cli.Command{
 			{
 				Name:      "show",
-				Usage:     "[INCOMPLETE] shows the local and remote branches. Can switch to the branch by entering the number",
+				Usage:     "shows the local and remote branches. Can switch to the branch by entering the number",
 				UsageText: "gg show",
 				Action: func(c *cli.Context) error {
 					message, err := commands.Show()
@@ -40,7 +40,7 @@ func main() {
 			},
 			{
 				Name:      "save",
-				Usage:     "[INCOMPLETE] Commit all working files with a given commit message. Usually followed by gg sync.",
+				Usage:     "Commit all working files with a given commit message. Usually followed by gg sync.",
 				UsageText: "gg sync <commit message>",
 				Action: func(c *cli.Context) error {
 					// combines all args into a single string so we don't need to use quotation marks
@@ -57,7 +57,7 @@ func main() {
 			},
 			{
 				Name:      "sync",
-				Usage:     "[INCOMPLETE] Pull any commits from your remote branch into your local, and push any local commits to the remote.",
+				Usage:     "Pull any commits from your remote branch into your local, and push any local commits to the remote.",
 				UsageText: "gg sync",
 				Action: func(c *cli.Context) error {
 					message, err := commands.Sync()
