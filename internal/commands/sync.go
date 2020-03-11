718e38e7cde3c6f661a4f95aee6d3d1f74489142
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-03-11 13:17:21 +1100

    Initial commit to set up remote upstream

--- internal/commands/sync.go
+++ internal/commands/sync.go
@@ -4,13 +4,43 @@ import (
 	"errors"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func Sync() (string, error) {
+	// get current branch
+	// call rev-parse on it
+	// if command errors then there's no remote branch and need to set the upstream
+	command := exec.Command("git", "branch", "--show-current")
+	output, err := command.CombinedOutput()
+
+	if err != nil {
+		return "", errors.New(bytesToString(output))
+	}
+
+	currentBranch := strings.TrimSpace(bytesToString(output))
+
+	command = exec.Command("git", "rev-parse", "--abbrev-ref", fmt.Sprintf("%v@{upstream}", currentBranch))
+	output, err = command.CombinedOutput()
+
+	if err != nil {
+		// then there's no upstream set for the branch
+		fmt.Println("No Remote branch set up for: ", currentBranch)
+
+		command = exec.Command("git", "push", "--set-upstream", "origin", currentBranch)
+		output, err = command.CombinedOutput()
+
+		if err != nil {
+			return "", errors.New(bytesToString(output))
+		}
+
+		return bytesToString(output), nil
+	}
+
 	fmt.Println("Step 1 of 2: Integrating remote changes into local...")
 
-	command := exec.Command("git", "pull")
-	output, err := command.CombinedOutput()
+	command = exec.Command("git", "pull")
+	output, err = command.CombinedOutput()
 
 	if err != nil {
 		return "", errors.New(bytesToString(output))
