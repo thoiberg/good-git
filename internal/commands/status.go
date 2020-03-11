b72f740a7b5ffffb7759b3992a93e29df9eedc5b
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-03-11 14:36:39 +1100

    Remove commented out code

--- internal/commands/status.go
+++ internal/commands/status.go
@@ -3,13 +3,6 @@ package commands
 import utils "github.com/thoiberg/good-git/internal/utils"
 
 func Status() (string, error) {
-	// command := exec.Command(
-	// 	"git",
-	// 	"-c", "color.status=always", // @see https://stackoverflow.com/a/18304605
-	// 	"status",
-	// )
-	// output, err := command.CombinedOutput()
-
 	output, err := utils.RunGitCommand("git -c color.status=always status")
 
 	if err != nil {
